65920f2b365012ef6f4fd140180ce2e7bddd007c
Jarvin <alwayzjarvin@gmail.com>
2026-02-23 16:49:07 +0100

    Initial Commit

---
+++ health-and-lifestyle/health_and_lifestyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_and_lifestyleApp.swift
+//  health-and-lifestyle
+//
+//  Created by Jarvin Siegers on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct health_and_lifestyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
